785067d91a4a0b9d9d73615f5747f7d3a5ac67ae
lampd <lampd.work@gmail.com>
2023-06-25 16:19:24 +0700

    fix: update chatGPT

--- chatGPT.js
+++ chatGPT.js
@@ -13,26 +13,23 @@ const chatGPTResponse = async (message, userId, client) => {
     .find({ userId })
     .sort({ createdAt: -1 })
     .limit(parseInt(limit))
+    .sort({ createdAt: 1 })
     .toArray()
-
-  console.log(chatHistoryResults, 'chatHistoryResults')
 
   const isEmptyChatHistory = !(chatHistoryResults?.length > 0)
 
   if (isEmptyChatHistory) {
     const firstMess = {
+      userId,
       role: 'assistant',
       content:
-        'Tôi là DLBot được tạo ra bởi Đăng Lâm và được tích hợp ChatGPT để trả lời các câu hỏi của bạn'
+        'Tôi là DLBot được tạo ra bởi Đăng Lâm và được tích hợp ChatGPT để trả lời các câu hỏi của bạn',
+      createdAt: new Date()
     }
 
     chatHistoryResults.push(firstMess)
 
-    collection.insertOne({
-      userId,
-      ...firstMess,
-      createdAt: new Date()
-    })
+    collection.insertOne(firstMess)
   }
 
   try {
@@ -42,34 +39,27 @@ const chatGPTResponse = async (message, userId, client) => {
 
     const res = await api.sendMessage(
       `Hãy trả lời tin nhắn của tôi dựa vào kiến thức của bạn hoặc tham khảo thêm thông tin từ lịch sử đoạn hội thoại của tôi và bạn. 
-      Đây là lịch sử đoạn hội thoại của tôi và bạn. bạn có role là assistant, còn tôi có role là "user", Đoạn hội thoại: ${JSON.stringify(
+      Đây là lịch sử đoạn hội thoại của tôi và bạn. bạn có role là assistant, còn tôi có role là user, Đoạn hội thoại: ${JSON.stringify(
         chatHistoryResults
       )}.
       Tin nhắn của tôi: ${message}`
     )
 
     const userMessage = {
+      userId,
       role: 'user',
       content: message,
       createdAt: new Date()
     }
 
     const assistantMessage = {
+      userId,
       role: 'assistant',
       content: res.text,
       createdAt: new Date()
     }
 
-    collection.insertMany([
-      {
-        userId,
-        ...userMessage
-      },
-      {
-        userId,
-        ...assistantMessage
-      }
-    ])
+    collection.insertMany([userMessage, assistantMessage])
 
     return res.text
   } catch (err) {
